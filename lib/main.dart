3f539112391f59d6605b799b53d156aca2414a52
Mahmoud Saber <saberm19071911@gmail.com>
2022-08-30 12:00:02 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'BmiScreen.dart';
+
+void main()
+{
+  runApp(MyApp());
+}
+
+// Stateless
+// Stateful
+
+// class MyApp
+
+class MyApp extends StatelessWidget
+{
+  // constructor
+  // build
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BmiScreen(),
+    );
+  }
+}
